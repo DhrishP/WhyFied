6d8458eea0721df8db3543ed2ebec0022d241f59
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2024-03-12 23:27:58 +0530

    changed prisma schema

--- app/admin/add-why/page.tsx
+++ app/admin/add-why/page.tsx
@@ -1,11 +1,64 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const AddQuestionsPage = () => {
-  return <>
-  <div>
-    
-  </div>
-  </>;
+  const modelArray = [
+    { id: "1", name: "Krishna" },
+    { id: "2", name: "Marcus" },
+    { id: "3", name: "Albert" },
+    { id: "4", name: "Seneca" },
+    { id: "5", name: "Nietzsche" },
+    { id: "6", name: "Epictetus" },
+    { id: "7", name: "Ayn" },
+  ];
+  const [selectedModel, setSelectedModel] = React.useState<{
+    id: string;
+    name: string;
+  } | null>({ id: "1", name: "Krishna" });
+  const [tagValue, setTagValue] = React.useState<string>("");
+  // const [prompt, setPrompt] = React.useState<string>("");
+  // const promptChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setPrompt(event.target.value);
+  // };
+  const handleTagChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTagValue(event.target.value);
+  };
+  const HandleSubmit = () => {
+    console.log("selectedModel", selectedModel);
+    console.log("tagValue", tagValue);
+  };
+
+  const handleModelChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedModelId = event.target.value;
+    const selectedModel = modelArray.find(
+      (model) => model.id === selectedModelId
+    );
+    setSelectedModel(selectedModel || null);
+    console.log(selectedModel);
+  };
+  if (!modelArray) return <div>Loading...</div>;
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-screen h-screen space-y-8 font-semibold">
+        {/* <div>
+          <input placeholder="Add a prompt" type="text" value={prompt} onChange={promptChange} />
+          <Button onClick={HandleSubmit}>Get Question</Button>
+        </div>
+        <div></div> */}
+        <select onChange={handleModelChange}></select>
+        <input
+          type="text"
+          value={tagValue}
+          onChange={handleTagChange}
+          className="py-4"
+          placeholder="enter the question"
+        />
+        <Button onClick={HandleSubmit}>Submit</Button>
+      </div>
+    </>
+  );
 };
 
 export default AddQuestionsPage;
---
+++ fetchers/get-models.ts
@@ -0,0 +1,15 @@
+import { currentUser } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+
+export default async function GetModels() {
+  const user = await currentUser();
+  const res = await prisma.userModels.findMany({
+    where: {
+      userId: user?.id,
+    },
+    include: {
+      model: true,
+    }
+  });
+  return res.map((x) => x.model);
+}
