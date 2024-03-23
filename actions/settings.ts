4838f0dae586dfe5dae763a4d5280749c4ff5160
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2024-03-24 02:32:00 +0530

    added ai sdk and fixed settings action

--- actions/settings.ts
+++ actions/settings.ts
@@ -3,7 +3,7 @@
 import * as z from "zod";
 import bcrypt from "bcryptjs";
 
-import { update } from "@/auth";
+import { unstable_update } from "@/auth";
 import { prisma } from "@/lib/prisma";
 import { SettingsSchema } from "@/schemas";
 import { getUserByEmail, getUserById } from "@/data/user";
@@ -11,19 +11,17 @@ import { currentUser } from "@/lib/auth";
 import { generateVerificationToken } from "@/lib/tokens";
 import { sendVerificationEmail } from "@/lib/mail";
 
-export const settings = async (
-  values: z.infer<typeof SettingsSchema>
-) => {
+export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
 
-  if (!user) {
-    return { error: "Unauthorized" }
+  if (!user?.id) {
+    return { error: "Unauthorized" };
   }
 
   const prismaUser = await getUserById(user.id);
 
   if (!prismaUser) {
-    return { error: "Unauthorized" }
+    return { error: "Unauthorized" };
   }
 
   if (user.isOAuth) {
@@ -37,15 +35,13 @@ export const settings = async (
     const existingUser = await getUserByEmail(values.email);
 
     if (existingUser && existingUser.id !== user.id) {
-      return { error: "Email already in use!" }
+      return { error: "Email already in use!" };
     }
 
-    const verificationToken = await generateVerificationToken(
-      values.email
-    );
+    const verificationToken = await generateVerificationToken(values.email);
     await sendVerificationEmail(
       verificationToken.email,
-      verificationToken.token,
+      verificationToken.token
     );
 
     return { success: "Verification email sent!" };
@@ -54,17 +50,14 @@ export const settings = async (
   if (values.password && values.newPassword && prismaUser.password) {
     const passwordsMatch = await bcrypt.compare(
       values.password,
-      prismaUser.password,
+      prismaUser.password
     );
 
     if (!passwordsMatch) {
       return { error: "Incorrect password!" };
     }
 
-    const hashedPassword = await bcrypt.hash(
-      values.newPassword,
-      10,
-    );
+    const hashedPassword = await bcrypt.hash(values.newPassword, 10);
     values.password = hashedPassword;
     values.newPassword = undefined;
   }
@@ -73,17 +66,17 @@ export const settings = async (
     where: { id: prismaUser.id },
     data: {
       ...values,
-    }
+    },
   });
 
-  update({
+  unstable_update({
     user: {
       name: updatedUser.name,
       email: updatedUser.email,
       isTwoFactorEnabled: updatedUser.isTwoFactorEnabled,
       role: updatedUser.role,
-    }
+    },
   });
 
-  return { success: "Settings Updated!" }
-}
+  return { success: "Settings Updated!" };
+};
