d177a4e74896c0b4ade82dcad7b630ddfe1d60df
Dhrish Parekh <parekhdhrish.pg@gmail.com>
2024-03-25 20:42:25 +0530

    Refactor add-note route to include multiple perspectives and update question timestamps

--- app/api/journal/add-note/route.ts
+++ app/api/journal/add-note/route.ts
@@ -3,8 +3,16 @@ import { prisma } from "@/lib/prisma";
 import { auth } from "@/auth";
 
 export async function POST(req: Request) {
-  const { note, questionId } = await req.json();
-  const session = await auth()
+  const {
+    perspective1,
+    perspective2,
+    perspective3,
+    perspective4,
+    perspective5,
+    questionId,
+    questionArray,
+  } = await req.json();
+  const session = await auth();
   if (!session?.user) {
     return NextResponse.json({ status: 401, body: "Unauthorized" });
   }
@@ -16,15 +24,19 @@ export async function POST(req: Request) {
   if (!user) {
     return NextResponse.json({ status: 401, body: "Unauthorized" });
   }
-  if (!note) {
+  if (!perspective1 || !perspective2) {
     return NextResponse.json({ status: 400, body: "Bad Request" });
   }
   const res = await prisma.journalEntries.create({
     data: {
       userId: user.id,
-      answer: note,
+      perspective1: perspective1,
+      perspective2: perspective2,
+      perspective3: perspective3,
+      perspective4: perspective4,
+      perspective5: perspective5,
       getQuestionId: questionId,
-      date: new Date(),
+      date: new Date().toLocaleDateString(),
     },
   });
   if (res) {
@@ -40,29 +52,44 @@ export async function POST(req: Request) {
     if (!res2) {
       return NextResponse.json({ status: 500, body: "ternal Server Error" });
     }
-    const res3 =await prisma.savequestionTimeStamps.update({
-      where:{
-        userId_timestamp:{
-          userId:user.id,
-          timestamp:new Date().toLocaleDateString()
-        
-        }
+    const res3 = await prisma.savequestionTimeStamps.update({
+      where: {
+        userId_timestamp: {
+          userId: user.id,
+          timestamp: new Date().toLocaleDateString(),
+        },
       },
-      data:{
-        isAnswered:true
-      }
-    })
-    if(!res3){
+      data: {
+        isAnswered: true,
+      },
+    });
+    if (!res3) {
       return NextResponse.json({ status: 500, body: "ternal Server Error" });
     }
     const res4 = await prisma.userQuestion.create({
-      data:{
+      data: {
         questionId,
-        userId:user.id
-      }
-    })
-    if(!res4){
+        userId: user.id,
+      },
+    }); 
+    if (!res4) {
       return NextResponse.json({ status: 500, body: "ternal Server Error" });
+    }
+    const res5 = await prisma.getQuestion.updateMany({
+      where: {
+        id: {
+          in: questionArray,
+        },
+      },
+      data: {
+        updatedAt: new Date(),
+      },
+    });
+    if (!res5) {
+      return NextResponse.json({
+        status: 500,
+        body: "Questions didnt get updated",
+      });
     }
     return NextResponse.json({ status: 200, body: "Answer Submitted" });
   }
